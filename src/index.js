f793c6970b77350610f3eadf632f6162be80fe76
tatitati <francis.albusac@simplybusiness.co.uk>
2020-09-25 09:05:17 +0100

    added some more components, changing approach :)

--- src/index.js
+++ src/index.js
@@ -2,40 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Clock from './Clock.js';
 import * as serviceWorker from './serviceWorker';
-
-/* global browser */
-browser.tabs.onActivated.addListener(event => update(event.tabId));
-browser.tabs.onUpdated.addListener(tabId => update(tabId));
-
-const themes = {
-    'live': {
-        colors: {
-            frame: "#CD3333"
-        }
-    }
-};
-
-var current = null;
-
-
-async function update(tabId) {
-    console.log('UPDATING.......')
-    if (current == null) {
-        current = await browser.theme.getCurrent();
-    }
-
-    let tab = await browser.tabs.get(tabId);
-    let windowId = tab.windowId;
-    var url = tab.url;
-    console.log("URL.....");
-    console.log(url);
-}
-
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+        <Clock />,
+        <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
