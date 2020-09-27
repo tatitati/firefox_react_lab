aed5603a64fb9ce562c10f4ab99ecfa749b050b1
tatitati <francis.albusac@simplybusiness.co.uk>
2020-09-27 18:32:28 +0100

    more

--- public/redify.js
+++ public/redify.js
@@ -7,21 +7,21 @@ const themes = {
 };
 
 browser.tabs.onHighlighted.addListener(event => update(event.tabIds[0]));
-// browser.tabs.onUpdated.addListener(tabId => update(tabId));
+browser.tabs.onUpdated.addListener(tabId => update(tabId));
 
 var currentTheme = null;
 
 async function update(tabId) {
-
     let tab = await browser.tabs.get(tabId);
-    let windowId = tab.windowId;
-    var urlTab = tab.url;
 
-    if (currentTheme == null) {
-        currentTheme = await browser.theme.getCurrent();
-    }
+    if (tab.active == true && tab.highlighted == true) {
+        let windowId = tab.windowId;
+        var urlTab = tab.url;
 
-    if (tab.active) {
+        if (currentTheme == null) {
+            currentTheme = await browser.theme.getCurrent();
+        }
+
         var storedDomains = []
         browser.storage.sync.get(['domains'], function (result) {
             if ('domains' in result) {
@@ -32,17 +32,14 @@ async function update(tabId) {
 
             var isLiveTab = false
             storedDomains.forEach(function (storedDomain) {
-                console.log(storedDomain + " vs " + urlTab)
                 if (urlTab.includes(storedDomain)) {
                     isLiveTab = true;
                 }
             })
 
             if (isLiveTab) {
-                console.log("is live!!!!")
                 browser.theme.update(windowId, themes.live);
             } else {
-                console.log("is not live")
                 browser.theme.update(windowId, currentTheme);
             }
         });
