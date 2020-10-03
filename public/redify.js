0a26a9f18648ed0183b6a4c5e378ee033fe94cd0
tatitati <francis.albusac@simplybusiness.co.uk>
2020-10-03 19:08:48 +0100

    simplification in code

--- public/redify.js
+++ public/redify.js
@@ -11,40 +11,89 @@ browser.tabs.onUpdated.addListener(tabId => handleThemeTab(tabId));
 
 var originalTheme = null;
 
+async function readOriginalTheme(){
+    if (originalTheme == null) {
+        current = await browser.theme.getCurrent();
+        if (current != themes.live) {
+            originalTheme = current
+        }
+    }
+
+}
+
+async function checkIfLive(urlTab){
+    let result = await browser.storage.sync.get(['domains'])
+    if ('domains' in result) {
+        storedDomains = result.domains
+    }
+
+    let isLiveTab = false
+    storedDomains.forEach(function (storedDomain) {
+        if (urlTab.includes(storedDomain)) {
+            isLiveTab = true;
+        }
+    })
+
+    return isLiveTab
+}
+
 async function handleThemeTab(tabId) {
     let tab = await browser.tabs.get(tabId);
 
     if (tab.active == true && tab.highlighted == true) {
         let windowId = tab.windowId;
-        var urlTab = tab.url;
 
-        if (originalTheme == null) {
-            current = await browser.theme.getCurrent();
-            if (current != themes.live) {
-                originalTheme = current
-            }
+        await readOriginalTheme()
+        let isLiveTab = await checkIfLive(tab.url)
+
+        if (isLiveTab) {
+            browser.theme.update(windowId, themes.live);
+        } else {
+            delete originalTheme.images // problem with images when restoring
+            browser.theme.update(windowId, originalTheme);
         }
-
-        var storedDomains = []
-        browser.storage.sync.get(['domains'], function (result) {
-            if ('domains' in result) {
-                storedDomains = result.domains
-            }
-
-            var isLiveTab = false
-            storedDomains.forEach(function (storedDomain) {
-                if (urlTab.includes(storedDomain)) {
-                    isLiveTab = true;
-                }
-            })
-
-            if (isLiveTab) {
-                browser.theme.update(windowId, themes.live);
-            } else {
-                delete originalTheme.images // problem with images when restoring
-                browser.theme.update(windowId, originalTheme);
-            }
-        });
     }
 }
+
+// async function handleUpdated(tabId, changeInfo, tab) {
+//     if (changeInfo.hasOwnProperty('url')) {
+//         let windowId = tab.windowId;
+//
+//         readOriginalTheme()
+//
+//         browser.storage.sync.get(['domains'], function (result) {
+//             if ('domains' in result) {
+//                 storedDomains = result.domains
+//             }
+//
+//             isLiveTab = false;
+//             if(tab.cookieStoreId != "firefox-container-6"){
+//                 storedDomains.forEach(function (storedDomain) {
+//                     if (tab.url.includes(storedDomain)) {
+//                         isLiveTab = true;
+//                     }
+//                 })
+//
+//                 if (isLiveTab) {
+//                     browser.tabs.remove(tab.id).then(function(){
+//                         browser.tabs.create(
+//                             {
+//                                 cookieStoreId: "firefox-container-6",
+//                                 url: tab.url
+//                             }
+//                         )
+//
+//                         console.log("upadting theme...")
+//                         browser.theme.update(windowId, themes.live);
+//                     })
+//
+//                 } else {
+//                     delete originalTheme.images // problem with images when restoring
+//                     browser.theme.update(windowId, originalTheme);
+//                 }
+//             }
+//         });
+//     }
+// };
+
   
